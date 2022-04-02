41684a30d117688fa8cf132f9f308c0b3fc79a91
Ronald_D_Bezerra <rebulgame@gmail.com>
2022-04-01 22:17:49 -0300

    adiciona rota Favorites

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './test.css';
 import Home from './pages/Home/Home'
 import HomeTunes from './pages/Tunes/HomeTunes';
 import SearchTunes from './pages/Tunes/page/SearchTunes';
+import Musics from './pages/Tunes/page/Musics';
+import Favorites from './pages/Tunes/page/Favorites';
 
 
 
@@ -15,6 +17,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/tunes" element={<HomeTunes />} />
       <Route path="/tunes/search" element={<SearchTunes /> } />
+      <Route path="/tunes/album/:id" element={<Musics />} />
+      <Route path='/tunes/favorites' element={<Favorites />} />
     </Routes>
     </>
   );
