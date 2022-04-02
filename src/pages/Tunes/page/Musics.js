6dd94f2a9b05fc9a917bf5f5cba76994eee92ffc
Ronald_D_Bezerra <rebulgame@gmail.com>
2022-04-01 22:20:25 -0300

    cria page Musics

---
+++ src/pages/Tunes/page/Musics.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from "react";
+import Header from "../components/Header";
+import MusicsCard from "../components/MusicsCard";
+
+function Musics () {
+  return (
+    <>
+    <Header />
+    <MusicsCard />
+    </>
+  )
+}
+
+export default Musics
