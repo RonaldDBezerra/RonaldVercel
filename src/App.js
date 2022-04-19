5ea6d91bfc02720a30d276796600affae9565037
Ronald_D_Bezerra <rebulgame@gmail.com>
2022-04-18 22:31:50 -0300

    remove rotas nao utilizadas

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import './mobilePort.css';
 import './test.css';
 import Home from './pages/Home/Home'
-import HomeTunes from './pages/Tunes/HomeTunes';
-import SearchTunes from './pages/Tunes/page/SearchTunes';
-import Musics from './pages/Tunes/page/Musics';
-import Favorites from './pages/Tunes/page/Favorites';
+
 
 
 
 function App() {
   return (
-    <>
+    <div className='pt-0'>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/tunes" element={<HomeTunes />} />
-      <Route path="/tunes/search" element={<SearchTunes /> } />
-      <Route path="/tunes/album/:id" element={<Musics />} />
-      <Route path='/tunes/favorites' element={<Favorites />} />
     </Routes>
-    </>
+    </div>
   );
 }
 
